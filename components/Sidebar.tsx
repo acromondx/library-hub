c0e0454297f62679fe0583bc22d993e947afdaca
AcromondX <50566307+acromondx@users.noreply.github.com>
2024-06-19 16:48:12 +0000

    Added: Dashboard Structures

---
+++ components/Sidebar.tsx
@@ -0,0 +1,118 @@
+"use client";
+import Link from "next/link";
+import { Bell, Home, LineChart, Package2, ShoppingCart } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { useSelectedLayoutSegment } from "next/navigation";
+import { cn } from "@/lib/utils";
+
+{
+  /* <Link
+href="#"
+className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+>
+<LineChart className="h-4 w-4" />
+Analyticsn
+</Link> */
+}
+
+// Home,
+// LineChart,
+// Menu,
+// Package,
+// Package2,
+// ShoppingCart,
+// Users,
+
+const navigation = [
+  { name: "Dashboard", icon: Home, href: "/" },
+  { name: "Orders", icon: ShoppingCart, href: "/orders" },
+  { name: "Products", icon: LineChart, href: "/products" },
+  { name: "Customers", icon: LineChart, href: "/customers" },
+  { name: "Analytics", icon: LineChart, href: "/analytics" },
+];
+
+export function Sidebar() {
+  const segment = useSelectedLayoutSegment();
+
+  return (
+    <div className="hidden border-r bg-muted/40 md:block">
+      <div className="flex h-full max-h-screen flex-col gap-2">
+        <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
+          <Link href="/" className="flex items-center gap-2 font-semibold">
+            <Package2 className="h-6 w-6" />
+            <span className="">Acme Inc</span>
+          </Link>
+          <Button variant="outline" size="icon" className="ml-auto h-8 w-8">
+            <Bell className="h-4 w-4" />
+            <span className="sr-only">Toggle notifications</span>
+          </Button>
+        </div>
+        <div className="flex-1">
+          <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
+            {navigation.map((item, index) => {
+              const Icon = item.icon;
+              let isActive = item.href.includes(String(segment));
+              return (
+                <Link
+                  aria-label={item.name}
+                  key={index}
+                  href={item.href}
+                  className={cn(
+                    item.href.includes(String(segment))
+                      ? "flex items-center gap-3 rounded-lg bg-muted px-3 py-2 text-primary transition-all hover:text-primary"
+                      : "flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary",
+                  )}
+                >
+                  <Icon className="h-4 w-4" />
+                  {item.name}
+                </Link>
+              );
+            })}
+          </nav>
+        </div>
+        <div className="mt-auto p-4">
+          <Card x-chunk="dashboard-02-chunk-0">
+            <CardHeader className="p-2 pt-0 md:p-4">
+              <CardTitle>Upgrade to Pro</CardTitle>
+              <CardDescription>
+                Unlock all features and get unlimited access to our support
+                team.
+              </CardDescription>
+            </CardHeader>
+            <CardContent className="p-2 pt-0 md:p-4 md:pt-0">
+              <Button size="sm" className="w-full">
+                Upgrade
+              </Button>
+            </CardContent>
+          </Card>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+{
+  /* <Link
+href="#"
+className="flex items-center gap-3 rounded-lg bg-muted px-3 py-2 text-primary transition-all hover:text-primary"
+>
+<Package className="h-4 w-4" />
+Products{" "}
+</Link>
+
+<Link
+href="#"
+className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+>
+<Users className="h-4 w-4" />
+Customers
+</Link> */
+}
