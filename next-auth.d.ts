782d3046ef971005119cc20114599a04283b1d60
AcromondX <50566307+acromondx@users.noreply.github.com>
2024-07-21 15:22:44 +0000

    Temp fix vercel deployment

--- next-auth.d.ts
+++ next-auth.d.ts
@@ -1,22 +1,21 @@
-import type { UserRole } from "@prisma/client";
-// biome-ignore lint/correctness/noUnusedImports: <Needed for nextauth type definition>
-import NextAuth from "next-auth";
-import type { User } from "next-auth";
-// biome-ignore lint/correctness/noUnusedImports: <Needed for nextauth type definition>
-import { JWT } from "next-auth/jwt";
+// /* eslint-disable unused-imports/no-unused-imports */
+// import type { UserRole } from "@prisma/client";
+// import NextAuth from "next-auth";
+// import type { User } from "next-auth";
+// import { JWT } from "next-auth/jwt";
 
-export type ExtendedUser = User & {
-  role: UserRole;
-};
+// export type ExtendedUser = User & {
+//   role: UserRole;
+// };
 
-declare module "next-auth/jwt" {
-  interface JWT {
-    role: UserRole;
-  }
-}
+// declare module "next-auth/jwt" {
+//   interface JWT {
+//     role: UserRole;
+//   }
+// }
 
-declare module "next-auth" {
-  interface Session {
-    user: ExtendedUser;
-  }
-}
+// declare module "next-auth" {
+//   interface Session {
+//     user: ExtendedUser;
+//   }
+// }
